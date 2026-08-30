7c2456a349a30e3cc503e0df6ad3dd4f4ae6f492
merrinx <gako.footwork856@passinbox.com>
2026-08-30 14:34:37 +0200

    refactor(treesit): use Emacs 31 mode activation

--- emacs/minimal/treesit.nix
+++ emacs/minimal/treesit.nix
@@ -2,29 +2,24 @@ _:
 {
   order = 902;
   elisp = ''
-    ;;; Tree-sitter (minimal: built-in ts-modes + nix)
     (use-package treesit
       :ensure nil
       :custom
       (treesit-font-lock-level 4)
+      (treesit-enabled-modes '(c-ts-mode
+                               css-ts-mode
+                               java-ts-mode
+                               js-ts-mode
+                               python-ts-mode
+                               bash-ts-mode
+                               yaml-ts-mode
+                               typescript-ts-mode
+                               tsx-ts-mode
+                               rust-ts-mode
+                               lua-ts-mode
+                               dockerfile-ts-mode))
       :config
-      ;; Redirect classic built-in modes to their tree-sitter counterparts.
-      (dolist (m '((c-mode . c-ts-mode)
-                   (css-mode . css-ts-mode)
-                   (java-mode . java-ts-mode)
-                   (js-mode . js-ts-mode)
-                   (javascript-mode . js-ts-mode)
-                   (python-mode . python-ts-mode)
-                   (sh-mode . bash-ts-mode)))
-        (add-to-list 'major-mode-remap-alist m))
-      ;; Languages whose classic major mode isn't bundled in the minimal set:
-      ;; bind the file extension straight to the built-in tree-sitter mode.
-      (dolist (a '(("\\.ya?ml\\'" . yaml-ts-mode)
-                   ("\\.ts\\'" . typescript-ts-mode)
-                   ("\\.tsx\\'" . tsx-ts-mode)
-                   ("\\.rs\\'" . rust-ts-mode)
-                   ("\\.lua\\'" . lua-ts-mode)
-                   ("Dockerfile\\'" . dockerfile-ts-mode)))
-        (add-to-list 'auto-mode-alist a)))
+      (unless (assoc "\\.mjs\\'" auto-mode-alist)
+        (add-to-list 'auto-mode-alist '("\\.mjs\\'" . js-ts-mode))))
   '';
 }
--- emacs/modules/languages/treesit.nix
+++ emacs/modules/languages/treesit.nix
@@ -2,27 +2,26 @@ _:
 {
   order = 902;
   elisp = ''
-    ;;; Setting up treesitter grammars
-      (use-package treesit
-        :ensure nil
-        :custom
-        (treesit-font-lock-level 4) ;; Set font lock level for Tree-sitter
-        :config
-        (seq-do (lambda (it)
-      	      (push it major-mode-remap-alist))
-      	    '((css-mode . css-ts-mode) ;; Remap CSS mode to Tree-sitter mode
-      	      (c-mode . c-ts-mode) ;; Remap C mode to Tree-sitter mode
-      	      (dockerfile-mode . dockerfile-ts-mode) ;; Remap Dockerfile mode to Tree-sitter mode
-      	      (haskell-mode . haskell-ts-mode) ;; Remap Haskell mode to Tree-sitter mode
-      	      (java-mode . java-ts-mode) ;; Remap Java mode to Tree-sitter mode
-      	      (javascript-mode . js-ts-mode) ;; Remap JavaScript mode to Tree-sitter mode
-      	      (kotlin-mode . kotlin-ts-mode) ;; Remap Kotlin mode to Tree-sitter mode
-      	      (lua-mode . lua-ts-mode) ;; Remap Lua mode to Tree-sitter mode
-      	      (python-mode . python-ts-mode) ;; Remap Python mode to Tree-sitter mode
-      	      (scala-mode . scala-ts-mode) ;; Remap Scala mode to Tree-sitter mode
-      	      (sh-mode . bash-ts-mode) ;; Remap Shell Script mode to Tree-sitter mode
-      	      (shell-script-mode . bash-ts-mode) ;; Remap Shell Script mode to Tree-sitter mode
-      	      (typescript-mode . typescript-ts-mode) ;; Remap TypeScript mode to Tree-sitter mode
-      	      (yaml-mode . yaml-ts-mode)))) ;; Remap YAML mode to Tree-sitter mode
+    (use-package treesit
+      :ensure nil
+      :custom
+      (treesit-font-lock-level 4)
+      (treesit-enabled-modes '(c-ts-mode
+                               css-ts-mode
+                               dockerfile-ts-mode
+                               java-ts-mode
+                               js-ts-mode
+                               lua-ts-mode
+                               python-ts-mode
+                               bash-ts-mode
+                               typescript-ts-mode
+                               yaml-ts-mode))
+      :config
+      (unless (assoc "\\.mjs\\'" auto-mode-alist)
+        (add-to-list 'auto-mode-alist '("\\.mjs\\'" . js-ts-mode)))
+      (dolist (m '((haskell-mode . haskell-ts-mode)
+                   (kotlin-mode . kotlin-ts-mode)
+                   (scala-mode . scala-ts-mode)))
+        (add-to-list 'major-mode-remap-alist m)))
   '';
 }
